e848efa40d37e479e62ac56e33a09ef3c5d85fda
Prasanth M S <servers@migobucks.com>
2022-03-07 12:14:19 +0530

    Header valiadtion added

--- app/handlers/health.ts
+++ app/handlers/health.ts
@@ -1,13 +1,24 @@
 import cors from "@middy/http-cors";
-import commonMidleware from "../utils/commonMidleware";
+import commonMidleware, {
+  MakeHeaderRequest,
+  ValidateHeader,
+} from "../utils/commonMidleware";
 
- 
-export const healthCheck= async(event:any, context:any)=> { 
+export const healthCheck = async (event: any, context: any) => {
+  let validateResponse = ValidateHeader(event["headers"]);
+  if (!validateResponse.Status) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(validateResponse),
+    };
+  }
+  const headerRequest = MakeHeaderRequest(event["headers"]);
 
-  let respose = {status:true, message: "Inventory Api Health Check Passed"}
+  console.log("Header", headerRequest);
+  let respose = { status: true, message: "Inventory Api Health Check Passed" };
   return {
     statusCode: 200,
     body: JSON.stringify(respose),
   };
-} 
- export const handler = commonMidleware(healthCheck).use(cors());;
+};
+export const handler = commonMidleware(healthCheck).use(cors());
--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,2 +1,9 @@
 export const ProductTable = "Products-dev"
 export const ProductCategoryTable = "ProductCategory-dev";
+
+export const HeaderConstants={
+    CustomerID:'X-MIBAPI-CustomerID',
+    CustomerType:'X-MIBAPI-CustomerType',
+    Source:'X-MIBAPI-Source',
+    Token:'X-MIBAPI-Token'
+}
